4ae2968c684ede71973bfb82e50795850658525b
Настенька <nastenka.kuzmina.1996@mail.ru>
2022-12-10 17:05:44 +0300

    CSh

--- CSh/Program.cs
+++ CSh/Program.cs
@@ -1,1 +1,22 @@
 ﻿
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+Console.Write("Введите массив данных через ',': ");
+string strLine = Console.ReadLine();
+string[] inArr = strLine.Split(",");
+string[] outArr = new string[0];
+
+for (int i = 0; i < inArr.Length; i++)
+{
+    if (inArr[i].Length <= 3)
+    {
+        Array.Resize(ref outArr, outArr.Length + 1);
+        outArr[outArr.Length-1] = inArr[i];
+    }
+
+}
+Console.WriteLine($"[{string.Join(",", inArr)}]=>[{string.Join(",", outArr)}]");
